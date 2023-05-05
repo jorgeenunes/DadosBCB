bbfd737468e78d4497c8d64b071f968c3fcfe8b9
jorgeenunes <jorgeeduardonunes@gmail.com>
2023-05-05 11:29:29 -0300

    Commit inicial do pacote no Github.

---
+++ R/handle_api_error.R
@@ -0,0 +1,24 @@
+#' Lidar com erros da API
+#'
+#'#' Função para lidar com possíveis erros na resposta da API
+#'
+#' @param res Resposta da API
+#'
+#' @return Retorna um erro com uma mensagem explicativa
+#' @export
+handle_api_error <- function(res) {
+  if (httr::http_type(res) == "application/json") {
+    msg <- httr::content(res, "text", encoding = "UTF-8")
+    if (length(msg) == 1) {
+      if (grepl("^\\{\"mensagem\".*", msg)) {
+        stop(paste0("Erro na API do Banco Central: ", msg))
+      }
+    }
+  }
+  else if (httr::http_type(res) == "text/html") {
+    stop("Erro na API do Banco Central: página não encontrada")
+  }
+  else {
+    stop("Erro na API do Banco Central: resposta inesperada")
+  }
+}
